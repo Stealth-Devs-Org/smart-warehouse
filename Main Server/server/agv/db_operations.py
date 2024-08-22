66a935948d50a2a21bd6a9f4b32a1eee99d55412
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-08-22 22:22:03 +0530

    refactor: Separate database operation in another file.

---
+++ Main Server/server/agv/db_operations.py
@@ -0,0 +1,11 @@
+from server import database
+
+
+def save_agv_location(data):
+    agv = {
+        "agv_id": data["agv_id"],
+        "location": data["location"],
+        "status": data["status"],  # 0: idle, 1: moving, 2: loading, 3: unloading
+        "timestamp": data["timestamp"],
+    }
+    database["agvs"].insert_one(agv)
