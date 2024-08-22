5ef056160faf6ebc2dc07750936dc707b18ad28a
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-08-22 21:39:41 +0530

    refactor: Refactor code with black formatter

--- Main Server/server/__init__.py
+++ Main Server/server/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from server.config import Config
 from pymongo import MongoClient
 
+from server.config import Config
 
 mongo_client = MongoClient(Config.MONGO_URI)
 database = mongo_client["warehouse_database"]
@@ -10,10 +10,9 @@ database = mongo_client["warehouse_database"]
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-    
-    
+
     from server.mqtt.utils import mqtt_client
+
     mqtt_client.init_app(app)
-    
-    
+
     return app
--- Main Server/server/config.py
+++ Main Server/server/config.py
@@ -1,14 +1,12 @@
+class Config:
+    SECRET_KEY = "dbdd91de1ea616b24740a3d963a7f95c"
 
-class Config :
-    SECRET_KEY = 'dbdd91de1ea616b24740a3d963a7f95c'
-    
-    MONGO_URI = 'mongodb://localhost:27017/'
-    
+    MONGO_URI = "mongodb://localhost:27017/"
+
     # MQTT configuration
     MQTT_BROKER_URL = "test.mosquitto.org"
     MQTT_BROKER_PORT = 1883
-    MQTT_USERNAME = ''
-    MQTT_PASSWORD = ''
+    MQTT_USERNAME = ""
+    MQTT_PASSWORD = ""
     MQTT_KEEPALIVE = 5
     MQTT_TLS_ENABLED = False
-    
