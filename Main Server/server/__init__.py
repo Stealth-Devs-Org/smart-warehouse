f5999fea421c3a2ff1f260c61758419a4563f5e8
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-08-22 08:22:50 +0530

    refactor: Removed unused SQlite DB

--- Main Server/server/__init__.py
+++ Main Server/server/__init__.py
@@ -1,16 +1,11 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from server.config import Config
-
-
-db = SQLAlchemy()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    db.init_app(app)
     
     from server.mqtt.utils import mqtt_client
     mqtt_client.init_app(app)
