187358adbb4f594fe6226fc760b471633536f099
SAIRISAN123 <sairisan123@gmail.com.com>
2024-08-30 15:35:40 +0530

    added AGV communication Script

--- Main Server/run.py
+++ Main Server/run.py
@@ -1,34 +1,6 @@
-from flask import Flask, request, jsonify
-from server import database  
+from server import create_app
 
-app = Flask(__name__)
-
-
-@app.route('/test', methods=['GET'])
-def test_endpoint():
-    return jsonify({"message": "Test endpoint is working!"}), 200
-
-
-# @app.route('/update_agv_location', methods=['POST'])
-# def update_agv_location():
-#     data = request.json
-#     print("Received data:", data)  # Debug log
-#     agv = {
-#         "agv_id": data["agv_id"],
-#         "location": data["location"],
-#         "segment": data["segment"],
-#         "status": data["status"],
-#         "timestamp": data["timestamp"],
-#     }
-#     database["agvs"].insert_one(agv)
-#     print("AGV location saved")  # Debug log
-#     return jsonify({"message": "AGV location updated"}), 200
-
-
-# @app.route('/get_agv_locations', methods=['GET'])
-# def get_agv_locations():
-#     agvs = list(database["agvs"].find({}))
-#     return jsonify(agvs), 200
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=False)
---
+++ Main Server/server/digitalWarehouse/AGV_Communication.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+agvs_data = {
+    "agv1": {
+        "agv_id": "agv1",
+        "location": [1, 2],
+        "segment": [[1, 2], [1, 3], [1, 4]],
+        "status": 1,
+        "timestamp": "2023-10-01T12:34:56Z",
+    },
+    "agv2": {
+        "agv_id": "agv2",
+        "location": [5, 3],
+        "segment": [[5, 3], [4, 3], [3, 3], [2, 3], [1, 3]],
+        "status": 1,
+        "timestamp": "2023-10-01T12:34:56Z",
+    },
+    "agv3": {
+        "agv_id": "agv3",
+        "location": [1, 4],
+        "segment": [[1, 4], [2, 4], [3, 4]],
+        "status": 1,
+        "timestamp": "2023-10-01T12:34:56Z",
+    },
+    "agv4": {
+        "agv_id": "agv4",
+        "location": [4, 5],
+        "segment": [[4, 5], [4, 6], [4, 7]],
+        "status": 1,
+        "timestamp": "2023-10-01T12:34:56Z",
+    },
+}
+
+@app.route('/AGV_Communications', methods=['GET'])
+def get_agvs():
+    return jsonify(agvs_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
