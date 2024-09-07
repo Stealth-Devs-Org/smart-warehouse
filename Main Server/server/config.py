5bdbcfaf88eaf718ab160eed3948dc58999f3a9f
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-09-07 17:12:12 +0530

    refactor: Update config.py to load environment variables from .env file

--- Main Server/server/config.py
+++ Main Server/server/config.py
@@ -1,12 +1,22 @@
-class Config:
-    SECRET_KEY = "dbdd91de1ea616b24740a3d963a7f95c"
+import os
 
-    MONGO_URI = "mongodb://localhost:27017/"
+import dotenv
+
+
+class Config:
+
+    project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+    dotenv_path = os.path.join(project_dir, ".env")
+    dotenv.load_dotenv(dotenv_path)
+
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    MONGO_URI = os.getenv("MONGO_URI")
 
     # MQTT configuration
-    MQTT_BROKER_URL = "localhost"
-    MQTT_BROKER_PORT = 1883
-    MQTT_USERNAME = ""
-    MQTT_PASSWORD = ""
-    MQTT_KEEPALIVE = 5
-    MQTT_TLS_ENABLED = False
+    MQTT_BROKER_URL = os.getenv("MQTT_BROKER_URL")
+    MQTT_BROKER_PORT = int(os.getenv("MQTT_BROKER_PORT"))
+    MQTT_USERNAME = os.getenv("MQTT_USERNAME")
+    MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
+    MQTT_KEEPALIVE = int(os.getenv("MQTT_KEEPALIVE"))
+    MQTT_TLS_ENABLED = os.getenv("MQTT_TLS_ENABLED", "false").lower() in ("true", "1", "yes")
