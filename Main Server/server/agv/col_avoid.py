3fb0413e5e3475691aabbfaa2543dc9d9fc2f470
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-09-26 11:17:46 +0530

    Refactor col_avoid.py to use concurrent.futures multiprocessing for collision avoidance

--- Main Server/server/agv/col_avoid.py
+++ Main Server/server/agv/col_avoid.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import json
 
 from flask import Blueprint, jsonify, render_template, request
@@ -91,7 +92,12 @@ def update_agv_location(data):
     agvs_data[data["agv_id"]] = data
     socketio.emit("agv_location", agvs_data)
     # print(agvs_data)
-    collision_avoidance(agvs_data)
+
+    # Use multiprocessing for CPU-bound collision avoidance
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.submit(collision_avoidance, agvs_data)
+
+    # collision_avoidance(agvs_data)
     save_agv_location(data)
 
 
