dc57a9e75d6765f5bbcb74b4f258121b9772e3ad
Rajaretnam Sairisan <sairisan123@gmail.com>
2024-09-27 06:58:38 +0530

    Refactor WebSocket server implementation

--- WebsocketTest/index.js
+++ WebsocketTest/index.js
@@ -1,26 +1,24 @@
 const WebSocket = require('ws');
-const port = process.env.PORT || 8080;  // Use a dynamic port for deployment platforms like Replit
+const port = process.env.PORT || 8080;  
 
-// Create WebSocket server
+
 const wss = new WebSocket.Server({ port });
 
-// When a client connects
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-  // Send a message to the connected client
-  ws.send('Welcome to the WebSocket server!');
 
-  // When a message is received from the client
+  ws.send('Welcome');
+
+ 
   ws.on('message', (message) => {
     console.log(`Received: ${message}`);
 
-    // Send a reply to the client
     ws.send(`You said: ${message}`);
   });
 
-  // Handle client disconnect
-  ws.on('close', () => {
+  
+  ws.on('close', () => {                    // Handle client disconnect
     console.log('Client disconnected');
   });
 });
