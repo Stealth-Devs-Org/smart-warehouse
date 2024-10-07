9de8198dc153d717ac7a952687dcaae369918d6d
Rajaretnam Sairisan <sairisan123@gmail.com>
2024-10-07 15:19:25 +0530

    Refactor TemperatureSensor class to remove unused code and update climate variable

--- Virtual Sensor Actuator/sensors/TemperatureSensor.py
+++ Virtual Sensor Actuator/sensors/TemperatureSensor.py
@@ -4,20 +4,6 @@ import time
 
 
 from sensorUtils import SetSensorState, sensor_state
-
-
-
-#sensor_state = {"sensor_type": "", "sensor_id": "", "sensor_location": "", "reading": 0.0, "current_status": 0}
-
-# def SetSensorState(type, id, location, reading, status):
-#     global sensor_state
-#     sensor_state["sensor_type"] = type
-#     sensor_state["sensor_id"] = id
-#     sensor_state["sensor_location"] = location
-#     sensor_state["reading"] = reading
-#     sensor_state["current_status"] = status     # 0 or 1 (0 = inactive, 1 = active)
-#     #print(f"Received new sensor state: {sensor_state}")
-
     
 climate = "winter"
 
