d68e752232ca4112f28faab81cba400385d65568
Clarance L.G.S. <sherongeorge11@gmail.com>
2024-10-16 16:05:44 +0530

    Update warehouseEnvironment.py

--- Virtual Sensor Actuator/sensors/warehouseEnvironment.py
+++ Virtual Sensor Actuator/sensors/warehouseEnvironment.py
@@ -3,11 +3,11 @@ import random
 
 warehouse_temperature_values = [25.0, 25.5, 24.5, 25.5, 23.5, 23.8, 23.7]
 
-warehouse_airquality_values = [0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.7]
+warehouse_airquality_values = [500, 520, 480, 700, 650, 580, 600] # in ppm
 
-warehouse_smoke_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.4, 0.3]
+warehouse_smoke_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.4, 0.3] # binaary value?
 
-warehouse_humidity_values = [0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.7]
+warehouse_humidity_values = [45.0, 47.5, 48.0, 49.5, 44.0, 50.2, 46.8]  # in %RH
 
 def main ():
     while True:
